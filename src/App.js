a736f9de3605fb246924226119a085b571eb3ae1
Sehajini Jayakody <98893191+sehajini1@users.noreply.github.com>
2024-06-29 13:06:24 +0530

    added tab wrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/LandingPage'
+
 
 function App() {
   return (
-    <div>
-      <p>Hi</p>
-    </div>
+    <>
+      <Router>
+        <Content>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+          </Routes>
+        </Content>
+      </Router>
+    </>
   );
 }
+const Content = styled.div`
+  ${
+    "" /* position: absolute;
+  margin-top: 10%; */
+  }
+`;
 
 export default App;
